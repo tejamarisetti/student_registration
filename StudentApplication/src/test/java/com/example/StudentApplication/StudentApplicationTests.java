771b9be9ab90de04e96d9eb8e5ad19b6e645d71a
Teja marisetti <tejugowd2005@gmail.com>
2026-04-08 16:29:12 +0530

    Add files via upload

---
+++ StudentApplication/src/test/java/com/example/StudentApplication/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
